8c960ddd31b5506677fa287e4f8b458439584309
Saurabh VR <72450609+saurabh1996-rex@users.noreply.github.com>
2021-05-17 20:26:45 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function red_btn(){
+    document.body.style.background="linear-gradient(to right top, #ff1328, #ff3848, #ff5263, #ff687d, #fb7d93)"
+}
+
+function yellow_btn(){
+    document.body.style.background="linear-gradient(to right top, #f9f903, #f4f830, #eff646, #eaf458, #e6f267)"
+
+}
+
+
+function green_btn(){
+    document.body.style.background="linear-gradient(to right top, #085c04, #187e09, #29a10f, #3bc515, #4feb19)"
+}
